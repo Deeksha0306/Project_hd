f17bc4532a1be62e0b172311fb2f8f8018a46e85
Deeksha <deekshak2103@gmail.com>
2025-02-24 15:14:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import streamlit as st
 import time as t
 from langchain_ollama.llms import OllamaLLM
 
-#Luigi model
-llama_model = OllamaLLM(model="deepseek-r1:1.5b")
+llama_model = OllamaLLM(model="smollm2")
 
-# Streamlit front
+
 st.title("Your buddy LUIGI 🤖")
 st.write("### Welcome! Ask me a question, and I'll try to help you 😎!")
 
-# Sidebar
+
 st.sidebar.title("AI Helpdesk")
 st.sidebar.selectbox("Why you came here?🤔", ["TimePass", "to Study", "to Chill"])
 st.sidebar.checkbox("I agree to the terms and conditions")
 
-# Input box for the prompt
 prompt = st.text_input("Ask me anything😏:")
 
 if prompt:
